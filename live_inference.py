9e064e04416fe4b93acfb4f8974dc0c26ea8d4c9
guppykang <guppykang@gmail.com>
2020-06-26 12:55:04 -0700

    pretty much done

--- live_inference.py
+++ live_inference.py
@@ -60,7 +60,7 @@ while True:
     frame = np.ascontiguousarray(frame) #since we changed it to a PIL image, so change back to [H, W, 3]
 
     for box_idx, box in enumerate(bounding_boxes):
-        cropped_face = frame[int(box[1]) : int(box[3]), int(box[0]) : int(box[2]), :] #maybe add a +/- 10 pixels here in case the bounding boxes are too strict
+        cropped_face = frame[int(box[1]) - 15 : int(box[3])+ 15, int(box[0]) - 15: int(box[2])+ 15, :] #maybe add a +/- 10 pixels here in case the bounding boxes are too strict
         aligned_face = alignment(cropped_face, landmarks[box_idx]) #crop and align the face to the preset landmark locations
         aligned_face = aligned_face.reshape((1,3,112,96)) 
 
@@ -68,11 +68,12 @@ while True:
         aligned_face = Variable(torch.from_numpy(aligned_face)).float().cuda()
 
         pred = net(aligned_face)[0]#.cpu().data.numpy().squeeze() #aint this line just satisfying
-        code.interact(local=locals())
+        _, pred_label = torch.max(pred.data, 1)
+        pred_label = pred_label.data[0]
 
-        pred_class = np.argmax(pred) 
-        print(pred_class)
-        if pred_class == 10575: #aka me
+        print(pred_label)
+        code.interact(local=locals())
+        if pred_label == 10575: #aka me
             print('me!')
 
         frame = cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (36,255,12), 1)
